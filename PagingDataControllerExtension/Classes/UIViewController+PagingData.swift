90ffe2fb76d623b1d29c86771d3ce16f45eb9a4f
Nguyen Chi Cong <congnc.if@gmail.com>
2016-09-29 15:12:59 +0700

    hidden scroll view footer loading when no more data

--- PagingDataControllerExtension/Classes/UIViewController+PagingData.swift
+++ PagingDataControllerExtension/Classes/UIViewController+PagingData.swift
@@ -18,36 +18,36 @@ public typealias PullHandler = ((() -> Swift.Void)? ) -> Swift.Void
  ********************************************************************************
  
  override func viewDidFinishLayout() {
-    super.viewDidFinishLayout()
-    
-    dataSource.settings = PageDataSettings(pageSize: provider.pageSize)
-    dataSource.delegate = self
-    
-    setupScrollViewForPaging(pullDownHandler: loadFirstPageWithCompletion, pullUpHandler: loadNextPageWithCompletion)
-    
-    showLoading()
-    loadFirstPageWithCompletion { [weak self] in
-        self?.pagingScrollView.reloadContent()
-        self?.hideLoading()
-    }
-}
+ super.viewDidFinishLayout()
  
-*********************************************************************************/
+ dataSource.settings = PageDataSettings(pageSize: provider.pageSize)
+ dataSource.delegate = self
+ 
+ setupScrollViewForPaging(pullDownHandler: loadFirstPageWithCompletion, pullUpHandler: loadNextPageWithCompletion)
+ 
+ showLoading()
+ loadFirstPageWithCompletion { [weak self] in
+ self?.pagingScrollView.reloadContent()
+ self?.hideLoading()
+ }
+ }
+ 
+ *********************************************************************************/
 
 extension UIViewController: PageDataSourceDelegate {
-
-	open var instantReloadContent: Bool {
-		return false
-	}
-
-	open var pagingScrollView: UIScrollView {
-		for subview in self.view.subviews {
-			if subview is UIScrollView {
-				return subview as! UIScrollView
-			}
-		}
-		fatalError("No scroll view in managed by \(self.classForCoder)")
-	}
+    
+    open var instantReloadContent: Bool {
+        return false
+    }
+    
+    open var pagingScrollView: UIScrollView {
+        for subview in self.view.subviews {
+            if subview is UIScrollView {
+                return subview as! UIScrollView
+            }
+        }
+        fatalError("No scroll view in managed by \(self.classForCoder)")
+    }
     
     // MARK: - Setup layout
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -56,30 +56,30 @@ extension UIViewController: PageDataSourceDelegate {
         self.setupPullToRefreshView(pullHandler: pullDownHandler)
         self.setupInfiniteScrollingView(pullHanlder: pullUpHandler)
     }
-
+    
     public func setupPullToRefreshView(pullHandler: @escaping PullHandler) {
-		_ = pagingScrollView.es_addPullToRefresh{ [weak self] in
+        _ = pagingScrollView.es_addPullToRefresh{ [weak self] in
             pullHandler({ [weak self] in
-				self?.pagingScrollView.reloadContent(instantReloadContent: (self?.instantReloadContent)!, end: {
-					[weak self] in
-					self?.pagingScrollView.es_stopPullToRefresh(completion: true)
-				})
-			})
-		}
-	}
+                self?.pagingScrollView.reloadContent(instantReloadContent: (self?.instantReloadContent)!, end: {
+                    [weak self] in
+                    self?.pagingScrollView.es_stopPullToRefresh(completion: true)
+                    })
+                })
+        }
+    }
     
     public func setupInfiniteScrollingView(pullHanlder: @escaping PullHandler) {
-		_ = pagingScrollView.es_addInfiniteScrolling { [weak self] in
+        _ = pagingScrollView.es_addInfiniteScrolling { [weak self] in
             pullHanlder({ [weak self] in
-				self?.pagingScrollView.reloadContent(instantReloadContent: (self?.instantReloadContent)!, end: {
-					[weak self] in
-					self?.pagingScrollView.es_stopLoadingMore()
-				})
-			})
-		}
-	}
-
-	// MARK: - Page Data Delegate
+                self?.pagingScrollView.reloadContent(instantReloadContent: (self?.instantReloadContent)!, end: {
+                    [weak self] in
+                    self?.pagingScrollView.es_stopLoadingMore()
+                    })
+                })
+        }
+    }
+    
+    // MARK: - Page Data Delegate
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
     
     public func pageDataSourceDidChanged(hasMoreFlag: Bool, changed: Bool) {
@@ -87,18 +87,61 @@ extension UIViewController: PageDataSourceDelegate {
     }
     
     // MARK: Functions
-	open func checkInfiniteView(_ hasMore: Bool) {
-		// call function with delay to fix mess animations
-		let delayTime = DispatchTime.now() + Double(Int64(0.25 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
-		DispatchQueue.main.asyncAfter(deadline: delayTime) { [weak self] in
+    open func checkInfiniteView(_ hasMore: Bool) {
+        // call function with delay to fix mess animations
+        let delayTime = DispatchTime.now() + Double(Int64(0.25 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+        DispatchQueue.main.asyncAfter(deadline: delayTime) { [weak self] in
+            
+            if (hasMore == false) {
+                self?.pagingScrollView.es_footer?.noMoreData = true
+                self?.pagingScrollView.es_footer?.isHidden = true
+            } else {
+                self?.pagingScrollView.es_footer?.noMoreData = false
+                self?.pagingScrollView.es_footer?.isHidden = false
+            }
+        }
+    }
+    
+    func startLoading() {
+        showLoading()
+    }
+    
+    func stopLoading() {
+        hideLoading()
+    }
+}
 
-			if (hasMore == false) {
-                self?.pagingScrollView.es_stopLoadingMore()
-			} else {
-                self?.pagingScrollView.es_resetNoMoreData()
-			}
-		}
-	}
+extension PagingControllerProtocol where Self: UIViewController {
+    
+    public func setupForPaging(loadFirstPage: Bool = true) {
+        
+        dataSource.settings = PageDataSettings(pageSize: provider.pageSize)
+        dataSource.delegate = self
+        
+        _ = pagingScrollView.es_addPullToRefresh { [weak self] in
+            self?.loadFirstPageWithCompletion({ [weak self] in
+                self?.pagingScrollView.reloadContent(instantReloadContent: (self?.instantReloadContent)!, end: {
+                    [weak self] in
+                    self?.pagingScrollView.es_stopPullToRefresh(completion: true)
+                    })
+                })
+        }
+        _ = pagingScrollView.es_addInfiniteScrolling { [weak self] in
+            self?.loadNextPageWithCompletion({ [weak self] in
+                self?.pagingScrollView.reloadContent(instantReloadContent: (self?.instantReloadContent)!, end: {
+                    [weak self] in
+                    self?.pagingScrollView.es_stopLoadingMore()
+                    })
+                })
+        }
+        
+        if loadFirstPage {
+            startLoading()
+            loadFirstPageWithCompletion({ [weak self] in
+                self?.pagingScrollView.reloadContent(instantReloadContent: (self?.instantReloadContent)!, end: self?.stopLoading)
+                })
+        }
+    }
 }
 
 
